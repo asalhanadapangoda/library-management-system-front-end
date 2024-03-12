50d9fe044c7fc7120046726aa929785776d97660
asalhanadapangoda <asalhimsanda@gmail.com>
2024-03-12 23:40:36 +0530

    Complete signin and signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
+import { ViweAllBooksComponent } from './page/viwe-all-books/viwe-all-books.component';
+import { RegisterComponent } from './page/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"login",
+        component:LoginComponent
+    },
+    {
+       path:"view-all-book",
+       component:ViweAllBooksComponent 
+    },
+    {
+        path:"sign-up",
+        component:RegisterComponent
+    }
+];
