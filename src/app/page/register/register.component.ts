370368c2119a24fbe7be2f61ef6509f83b5c362d
asalhanadapangoda <asalhimsanda@gmail.com>
2024-03-27 19:06:24 +0530

    Completed SignIn from

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -15,6 +15,18 @@ export class RegisterComponent implements OnInit{
 private http;
 public countryList:any;
 public selectedCountry:any;
+public isExistUser:any;
+public selectedCountryCode:any;
+public userObj={
+ firstName:null,
+ lastName:null,
+ userName:null,
+ email:null,
+ address:null,
+ address2:null,
+ country:null,
+ phoneNumber:null
+}
 
 constructor(private httpCliant:HttpClient){
   this.http=httpCliant;
@@ -31,6 +43,25 @@ loadCountries(){
 }
 setSlectedCountry(country:any){
 this.selectedCountry=country;
-  console.log(this.selectedCountry);
+this.selectedCountryCode=country.idd.root+""+country.idd.suffixes[0];
+  console.log(this.selectedCountryCode);
+}
+submitFrom(){
+ console.log(this.userObj);
+ this.http.get(`http://localhost:8080/user/is-Exists-User/${this.userObj.userName}`).subscribe(data=>{
+  console.log(data);
+  this.isExistUser=data;
+  this.registerUser(this.isExistUser)
+ })
+}
+registerUser(isExistUser:any){
+if(!isExistUser==true){
+  this.http.post("http://localhost:8080/user/add-user",this.userObj).subscribe(data=>{
+    alert(this.userObj.userName + " has been registered!");
+
+  })
+}else{
+  alert(this.userObj.userName + " has been already taken !");
+}
 }
 }
